083515b71d620fab8eea925beb0a207781c82c6e
Punkakes <bugeat.bastien@hotmail.fr>
2016-07-06 14:26:14 +0200

    Update UserController.java

--- src/main/java/hello/controller/UserController.java
+++ src/main/java/hello/controller/UserController.java
@@ -1,5 +1,8 @@
 package hello.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import hello.model.User;
 import hello.model.UserDao;
 
@@ -7,8 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
   // ------------------------
@@ -22,9 +26,9 @@ public class UserController {
    * @param name User's name
    * @return A string describing if the user is succesfully created or not.
    */
-  @RequestMapping("/create")
+  @RequestMapping("/invite")
   @ResponseBody
-  public String create(String email, String name) {
+  public String invite(String email, String name) {
     User user = null;
     try {
       user = new User(email, name);
@@ -33,8 +37,42 @@ public class UserController {
     catch (Exception ex) {
       return "Error creating the user: " + ex.toString();
     }
-    return "User succesfully created! (id = " + user.getId() + user + ")";
+    return "User succesfully created and will be invited soon! (  id = " + user.getId() + " email = " + 
+    user.getEmail() + " Name = " + user.getName() + ")";
   }
+  
+  @RequestMapping("/QuickCreate")
+  @ResponseBody
+  public String QuickCreate(String email, String name, String password) {
+    User user = null;
+    try {
+      user = new User(email, name, password);
+      userDao.save(user);
+    }
+    catch (Exception ex) {
+      return "Error creating the user: " + ex.toString();
+    }
+    return "User succesfully created! (  id = " + user.getId() + " email = " + 
+    user.getEmail() + " Name = " + user.getName() + user.getPassword() + ")";
+  }
+  
+  
+
+  /** 
+   * /UserList --> send the user list
+   * @return the JSON list of the user
+   
+  
+  @RequestMapping("UserList")
+  @ResponseBody
+  public List<UserDao>  UserList() {      
+      List<UserDao> userlist= new ArrayList<UserDao>();        
+      userlist = UserDao.getAllUser();
+      return userlist;
+  }
+  */
+  
+  
   
   /**
    * /delete  --> Delete the user having the passed id.
@@ -42,9 +80,6 @@ public class UserController {
    * @param id The id of the user to delete
    * @return A string describing if the user is succesfully deleted or not.
    */
-  
-  
-  
   
   @RequestMapping("/delete")
   @ResponseBody
@@ -69,15 +104,73 @@ public class UserController {
   @ResponseBody
   public String getByEmail(String email) {
     String userId;
+    String sender;
     try {
       User user = userDao.findByEmail(email);
       userId = String.valueOf(user.getId());
+      sender = "The user id is: " + userId;
     }
     catch (Exception ex) {
       return "User not found";
     }
-    return "The user id is: " + userId;
+    return String.format(sender, userId);
   }
+ 
+  /**
+  @RequestMapping("/alluser")
+  @ResponseBody
+  public List<User> userlist (){
+	  
+	return null;
+	  
+  }
+  
+  */
+  
+  @RequestMapping("/get-by-name")
+  @ResponseBody
+  public String getByName(String name) {
+    String userId;
+    String useremail;
+    String username; 
+   /** String userobj;*/
+    try {
+      User user = userDao.findByName(name);
+      userId = String.valueOf(user.getId());
+      useremail = String.valueOf(user.getEmail());
+      username = String.valueOf(user.getName());
+      /**userobj = String.valueOf(user.toString);*/
+    }
+    catch (Exception ex) {
+      return "User not found";
+    }
+    return "User found.  Name = "+ username + ", "
+    		+ " Id = "+ userId + ",  Email = " + useremail  ;
+  }
+  
+  
+  /** Authentication JAVA style
+   * @param email
+   * @param password
+   * @return result of the authentication
+   */
+  
+  @RequestMapping("/auth")
+  @ResponseBody
+  public String auth(String email, String password) {
+    String userId;
+    String username;
+    try {
+      User user = userDao.findByEmailAndPassword(email, password);
+      userId = String.valueOf(user.getId());
+      username = String.valueOf(user.getName());
+    }
+    catch (Exception ex) {
+      return "User or password wrong check it and retry please";
+    }
+    return "Connection succesful; Welcome home Mr " + username + ", ID = " + userId ;
+  }
+  
   
   /**
    * /update  --> Update the email and the name for the user in the database 
